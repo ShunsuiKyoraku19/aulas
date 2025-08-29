7c64de55de9d47254c61a5c574b792b46521d6da
ShunsuiKyoraku19 <lucasmaxwell297@gmail.com>
2025-08-29 00:04:26 -0300

    Add files via upload

---
+++ aulas/Paper_scisor_rock.js
@@ -0,0 +1,75 @@
+var frase = document.getElementById("frase");
+var jogador = document.getElementById("jogador");
+var computador = document.getElementById("computador");
+
+var botao = document.getElementById("botao");
+
+var areaEscolha = document.getElementById("area-escolha");
+var areaResultado = document.getElementById("area-resultado");
+
+var imagens_jogador = ["img/Rock.png", "img/Paper.png", "img/Scissors.png"];
+var imagens_computador = ["img/Rock-pc.png", "img/Paper-pc.png", "img/Scissors-pc.png"];
+
+var index = 0;
+
+function effect(){
+    jogador.src = imagens_jogador[index];
+    computador.src = imagens_computador[index];
+
+    index++;
+    if(index === 3){
+        index = 0;
+    }
+}
+var efeito = setInterval(effect, 100);
+
+function select(escolhaJogador){
+    areaEscolha.style.display = 'none';
+
+    areaResultado.style.display = 'block';
+
+    frase.textContent = '3';
+
+    var tempo = setInterval(()=>{
+        var cronometro = parseInt(frase.textContent);
+        cronometro --;
+        frase.textContent = cronometro;
+
+        if(cronometro === 0){
+            clearInterval(tempo);
+            clearInterval(efeito);
+        }
+    }, 1000);
+
+    setTimeout(() => {
+        var escolhaComputador = Math.floor(Math.random() * 3);
+        
+        computador.src = imagens_computador[escolhaComputador];
+        jogador.src = imagens_jogador[escolhaJogador];
+
+        if(escolhaJogador === escolhaComputador){
+            frase.textContent = 'Empatou!';
+            botao.style.display = 'block';
+
+            return false;
+        }
+
+        switch(escolhaJogador){
+            case 0://escolheu pedra
+            escolhaComputador === 2 ? frase.textContent = "Jogador venceu!" : frase.textContent = "Computador venceu!";
+            botao.style.display = 'block';
+            break;
+            case 1://escolheu papel
+            escolhaComputador === 0 ? frase.textContent = "Jogador venceu!" : frase.textContent = "Computador venceu!";
+            botao.style.display = 'block';
+            break;
+            case 2://escolheu tesoura
+            escolhaComputador === 1? frase.textContent = "Jogador venceu!" : frase.textContent = "Computador venceu!";
+            botao.style.display = 'block';
+            break;
+            default:
+                alert("Escolha invalida");
+        }
+    }, 3000);
+}
+
